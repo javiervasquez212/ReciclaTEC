8cee25ce24ec723f72942bc63d57dec3be44ae8a
Javier Vasquez <javiervasquez212@gmail.com>
2023-09-22 16:12:59 -0600

    fix: Deleted dona image

--- recicla-tec/src/pages/page-Main/mainPage.js
+++ recicla-tec/src/pages/page-Main/mainPage.js
@@ -1,27 +1,27 @@
-import React, { Component } from 'react'
-import './mainPage.css'
-import NavPrincipal from '../../components/cmp-navPrincipal/NavPrincipal.js'
-import CajaReciclaje from '../../components/componente-reciclar/CajaReciclaje.js'
+import React, { Component } from "react";
+import "./mainPage.css";
+import NavPrincipal from "../../components/cmp-navPrincipal/NavPrincipal.js";
+import CajaReciclaje from "../../components/componente-reciclar/CajaReciclaje.js";
 export default class mainPage extends Component {
-
-    render() {
-      return (
-        <>
-        <div className='main-page-all'>
-          <div> <NavPrincipal/> </div>
-          
-          <div className='reciclaje-caja'> <CajaReciclaje/> </div>
-          
-          <div className='componente-final'>
-            <h1>Material reciclado</h1>
-            <img src={require('../../Imagenes/Foto-Dona.png')} alt="Dona" />
+  render() {
+    return (
+      <>
+        <div className="main-page-all">
+          <div>
+            {" "}
+            <NavPrincipal />{" "}
           </div>
 
+          <div className="reciclaje-caja">
+            {" "}
+            <CajaReciclaje />{" "}
+          </div>
 
-
+          <div className="componente-final">
+            <h1>Material reciclado</h1>
+          </div>
         </div>
-        
-        </>
-      )
-    }
+      </>
+    );
   }
+}
