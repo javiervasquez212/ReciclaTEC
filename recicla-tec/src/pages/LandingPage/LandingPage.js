053f53aa8948a316e0aca80e80c7cf2df8a26eda
Javier Vasquez <javiervasquez212@gmail.com>
2023-09-29 20:46:01 -0600

    Feat: linktree connection

--- recicla-tec/src/pages/LandingPage/LandingPage.js
+++ recicla-tec/src/pages/LandingPage/LandingPage.js
@@ -4,13 +4,25 @@ import GaselLogo from "../../Img/Foto-Gasel.ico";
 import MainTopBar from "../../components/MainTopBar/MainTopBar";
 
 const LandingPage = () => {
+  // Función para redirigir al hacer clic en el logo
+  const redirectToLinktree = () => {
+    window.location.href = "https://linktr.ee/GaselTEC_Ambiente";
+  };
+
   return (
     <div>
       <MainTopBar />
       <div className="landing-page">
-        <div className="centered-image">
-          <img src={GaselLogo} />
-        </div>
+        {/* Usamos la etiqueta <a> para redirigir */}
+        <a
+          href="https://linktr.ee/GaselTEC_Ambiente"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <div className="centered-image" onClick={redirectToLinktree}>
+            <img src={GaselLogo} alt="Gasel Logo" />
+          </div>
+        </a>
       </div>
     </div>
   );
