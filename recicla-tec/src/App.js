dbd80afd75f434eec80dbdb5e0fee98884e87ab7
Javier Vasquez <javiervasquez212@gmail.com>
2023-09-29 18:40:24 -0600

    Deleted: Reset

--- recicla-tec/src/App.js
+++ recicla-tec/src/App.js
@@ -1,37 +1,17 @@
-import './App.css';
-import Login from './pages/page-Log-Sign/Login.js'
-import SignUp from './pages/page-Log-Sign/SignUp.js'
-import Home from './pages/page-Home/Home'
-import Developers from './pages/page-Developers/Developers.js';
-import Main from './pages/page-Main/mainPage.js';
-import Menu from './pages/page-Main/menuHamburguer'
-import Historial from './pages/page-Historial/Historial.js';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import "./App.css";
 
-// <Navigation/>
-//<Routes>
-// <Route path="/Login" component={<Login/>}/>
-//<Route path="/SignUp" component={<SignUp/>}/>
-// </Routes>
-//
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 function App() {
- return (
-    <div className='App'>
-    <Router>
-      <Routes>
-      <Route path="/" element={<Home />}/>
-        <Route path="/login" element={<Login />} />
-        <Route path="/signUp" element={<SignUp />} />
-        <Route path="/desarrolladores" element={<Developers />} />
-        <Route path="/main" element={<Main />} />
-        <Route path="/menu" element={<Menu />}/>
-        <Route path="/historial" element={<Historial />}/>
-        {/* Otras rutas */}
-      </Routes>
-    </Router>
-    
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
     </div>
- );
+  );
 }
 
 export default App;
