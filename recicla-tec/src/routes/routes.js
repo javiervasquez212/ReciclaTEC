e95b4422e8fd17d8cfd9fdba369e7747a609e923
Javier Vasquez <javiervasquez212@gmail.com>
2023-09-29 20:39:53 -0600

    Fix: fixed routes.js routes

--- recicla-tec/src/routes/routes.js
+++ recicla-tec/src/routes/routes.js
@@ -1,3 +1,7 @@
 import LandingPage from "../pages/LandingPage/LandingPage";
+import Login from "../pages/LoginPage/LoginPage";
 
-export const routes = [{ path: "/", element: <LandingPage /> }];
+export const routes = [
+  { path: "/", element: <LandingPage /> },
+  { path: "/Login", element: <Login /> },
+];
