c520096376182bfcd579f84ee8b152be8d5ce2aa
Javier Vasquez <javiervasquez212@gmail.com>
2023-09-29 20:39:26 -0600

    Fix: Rename to MainTopBar

--- recicla-tec/src/components/MainTopBar/MainTopBar.js
+++ recicla-tec/src/components/MainTopBar/MainTopBar.js
@@ -3,11 +3,16 @@ import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
+import { Link } from "react-router-dom"; // Importar el componente Link
 
 function TopBar() {
   const appBarStyle = {
-    backgroundColor: "green", // Establecer el color de fondo en verde
+    backgroundColor: "green",
     height: 60,
+  };
+  const linkStyle = {
+    textDecoration: "none",
+    color: "white", // Establecer el color del texto en blanco
   };
 
   return (
@@ -17,7 +22,9 @@ function TopBar() {
           ReciclaTEC
         </Typography>
         <div style={{ marginLeft: "auto" }}>
-          <Button color="inherit">Login</Button>
+          <Link to="/Login" style={linkStyle}>
+            <Button color="inherit">Login</Button>
+          </Link>
           <Button color="inherit">Register</Button>
         </div>
       </Toolbar>
