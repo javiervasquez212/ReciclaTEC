cac5253f568fb1766c53b202d5df43e824394149
Javier Vasquez <javiervasquez212@gmail.com>
2023-09-29 20:16:13 -0600

    Feat: Fixed LandingPage

--- recicla-tec/src/pages/LandingPage/LandingPage.js
+++ recicla-tec/src/pages/LandingPage/LandingPage.js
@@ -1,10 +1,20 @@
+import React from "react";
 import "./LandingPage.css";
+import "../../components/TopBar/TopBar";
+import TopBar from "../../components/TopBar/TopBar";
+import GaselLogo from "../../Img/Foto-Gasel.ico";
 
 const LandingPage = () => {
   return (
     <div>
-      <h1>hola</h1>
+      <TopBar />
+      <div className="landing-page">
+        <div className="centered-image">
+          <img src={GaselLogo} />
+        </div>
+      </div>
     </div>
   );
 };
+
 export default LandingPage;
