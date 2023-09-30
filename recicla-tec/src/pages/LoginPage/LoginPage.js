62eb898a61a90700ab5b305f79848c88ede0ec6d
Javier Vasquez <javiervasquez212@gmail.com>
2023-09-29 20:40:03 -0600

    Fix: fixed loginPage

---
+++ recicla-tec/src/pages/LoginPage/LoginPage.js
@@ -0,0 +1,70 @@
+import React from "react";
+import "./LoginPage.css";
+import { Container, Paper, TextField, Button, Typography } from "@mui/material";
+
+const Login = () => {
+  return (
+    <div className="landing-page">
+      <Container
+        component="main"
+        maxWidth="xs"
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          height: "100vh",
+        }}
+      >
+        <Paper
+          elevation={3}
+          sx={{
+            padding: 3,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            bgcolor: "#E0E0E0",
+          }}
+        >
+          <Typography component="h1" variant="h5" sx={{ color: "green" }}>
+            Iniciar sesión
+          </Typography>
+          <form sx={{ width: "100%", mt: 2 }}>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Correo electrónico"
+              name="email"
+              autoComplete="email"
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Contraseña"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              sx={{ mt: 2, bgcolor: "green", color: "white" }}
+            >
+              Iniciar sesión
+            </Button>
+          </form>
+        </Paper>
+      </Container>
+    </div>
+  );
+};
+
+export default Login;
