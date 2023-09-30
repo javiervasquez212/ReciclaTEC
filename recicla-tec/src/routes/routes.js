cd6f4fceda9a6c7b926ed6950ffa25b0c9fc3eab
Javier Vasquez <javiervasquez212@gmail.com>
2023-09-29 21:33:22 -0600

    Added: new routes

--- recicla-tec/src/routes/routes.js
+++ recicla-tec/src/routes/routes.js
@@ -1,7 +1,13 @@
 import LandingPage from "../pages/LandingPage/LandingPage";
 import Login from "../pages/LoginPage/LoginPage";
+import Register from "../pages/RegisterPage/RegisterPage";
+import RecyclingLog from "../pages/LogsPage/LogsPage";
+import RecyclingChart from "../pages/ChartsPage/ChartsPage";
 
 export const routes = [
   { path: "/", element: <LandingPage /> },
   { path: "/Login", element: <Login /> },
+  { path: "/Register", element: <Register /> },
+  { path: "/RecyclingLog", element: <RecyclingLog /> },
+  { path: "/RecyclingChart", element: <RecyclingChart /> },
 ];
