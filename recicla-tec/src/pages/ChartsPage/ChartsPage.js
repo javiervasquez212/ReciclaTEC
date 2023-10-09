3bbc08ae1bdc952a3a7acff0636b80169159bcdb
Javier Vasquez <javiervasquez212@gmail.com>
2023-10-09 17:19:52 -0600

    feat: Added two charts to ChartsPage

--- recicla-tec/src/pages/ChartsPage/ChartsPage.js
+++ recicla-tec/src/pages/ChartsPage/ChartsPage.js
@@ -3,26 +3,127 @@ import MainTopBar from "../../components/MainTopBar/MainTopBar";
 import "./ChartsPage.css";
 import { BarChart } from "@mui/x-charts/BarChart";
 
+const valueFormatter = (value: number) => `${value}mm`;
+const chartSetting = {
+  xAxis: [
+    {
+      label: "rainfall (mm)",
+    },
+  ],
+  width: 500,
+  height: 400,
+};
+const dataset = [
+  {
+    london: 59,
+    paris: 57,
+    newYork: 86,
+    seoul: 21,
+    month: "Jan",
+  },
+  {
+    london: 50,
+    paris: 52,
+    newYork: 78,
+    seoul: 28,
+    month: "Fev",
+  },
+  {
+    london: 47,
+    paris: 53,
+    newYork: 106,
+    seoul: 41,
+    month: "Mar",
+  },
+  {
+    london: 54,
+    paris: 56,
+    newYork: 92,
+    seoul: 73,
+    month: "Apr",
+  },
+  {
+    london: 57,
+    paris: 69,
+    newYork: 92,
+    seoul: 99,
+    month: "May",
+  },
+  {
+    london: 60,
+    paris: 63,
+    newYork: 103,
+    seoul: 144,
+    month: "June",
+  },
+  {
+    london: 59,
+    paris: 60,
+    newYork: 105,
+    seoul: 319,
+    month: "July",
+  },
+  {
+    london: 65,
+    paris: 60,
+    newYork: 106,
+    seoul: 249,
+    month: "Aug",
+  },
+  {
+    london: 51,
+    paris: 51,
+    newYork: 95,
+    seoul: 131,
+    month: "Sept",
+  },
+  {
+    london: 60,
+    paris: 65,
+    newYork: 97,
+    seoul: 55,
+    month: "Oct",
+  },
+  {
+    london: 67,
+    paris: 64,
+    newYork: 76,
+    seoul: 48,
+    month: "Nov",
+  },
+  {
+    london: 61,
+    paris: 70,
+    newYork: 103,
+    seoul: 25,
+    month: "Dec",
+  },
+];
+
 const RecyclingChart = () => {
   return (
     <div>
       <MainTopBar />
       <div className="chart-page">
         <BarChart
-          xAxis={[
-            {
-              id: "barCategories",
-              data: ["bar A", "bar B", "bar C"],
-              scaleType: "band",
-            },
-          ]}
           series={[
-            {
-              data: [2, 5, 3],
-            },
+            { data: [3, 4, 1, 6, 5], stack: "A", label: "series A1" },
+            { data: [4, 3, 1, 5, 8], stack: "A", label: "series A2" },
+            { data: [4, 2, 5, 4, 1], stack: "B", label: "series B1" },
+            { data: [2, 8, 1, 3, 1], stack: "B", label: "series B2" },
+            { data: [10, 6, 5, 8, 9], label: "series C1" },
           ]}
-          width={500}
-          height={300}
+          width={600}
+          height={350}
+        />
+        <BarChart
+          dataset={dataset}
+          yAxis={[{ scaleType: "band", dataKey: "month" }]}
+          series={[
+            { dataKey: "seoul", label: "Seoul rainfall", valueFormatter },
+          ]}
+          layout="horizontal"
+          {...chartSetting}
         />
       </div>
     </div>
