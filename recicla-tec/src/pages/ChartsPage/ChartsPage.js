09bca060a777ae2f62511b3de78371d08f5330f9
Javier Vasquez <javiervasquez212@gmail.com>
2023-10-09 12:54:12 -0600

    feat: Added graph

--- recicla-tec/src/pages/ChartsPage/ChartsPage.js
+++ recicla-tec/src/pages/ChartsPage/ChartsPage.js
@@ -1,12 +1,30 @@
 import React from "react";
 import MainTopBar from "../../components/MainTopBar/MainTopBar";
 import "./ChartsPage.css";
+import { BarChart } from "@mui/x-charts/BarChart";
 
 const RecyclingChart = () => {
   return (
     <div>
       <MainTopBar />
-      <div className="chart-page"></div>
+      <div className="chart-page">
+        <BarChart
+          xAxis={[
+            {
+              id: "barCategories",
+              data: ["bar A", "bar B", "bar C"],
+              scaleType: "band",
+            },
+          ]}
+          series={[
+            {
+              data: [2, 5, 3],
+            },
+          ]}
+          width={500}
+          height={300}
+        />
+      </div>
     </div>
   );
 };
