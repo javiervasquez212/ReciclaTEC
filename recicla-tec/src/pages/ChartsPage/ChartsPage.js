6c62e41ff4f71d08db3731325d1ff8ca566fc6c3
Javier Vasquez <javiervasquez212@gmail.com>
2023-10-09 12:37:46 -0600

    feat: ChartPage estructure

--- recicla-tec/src/pages/ChartsPage/ChartsPage.js
+++ recicla-tec/src/pages/ChartsPage/ChartsPage.js
@@ -1,39 +1,12 @@
 import React from "react";
-import { Doughnut } from "react-chartjs-2";
+import MainTopBar from "../../components/MainTopBar/MainTopBar";
+import "./ChartsPage.css";
 
-const RecyclingChart = ({ recycledItems }) => {
-  // Calcular la cantidad de cada tipo de material reciclado
-  const materialCount = {};
-  recycledItems.forEach((item) => {
-    if (materialCount[item.material]) {
-      materialCount[item.material] += item.quantity;
-    } else {
-      materialCount[item.material] = item.quantity;
-    }
-  });
-
-  // Crear un arreglo de colores para los segmentos del gráfico
-  const colors = ["#FF5733", "#33FF57", "#5733FF", "#FFFF33", "#33FFFF"];
-
-  // Crear los datos para el gráfico de dona
-  const data = {
-    labels: Object.keys(materialCount),
-    datasets: [
-      {
-        data: Object.values(materialCount),
-        backgroundColor: colors,
-      },
-    ],
-  };
-
-  const options = {
-    responsive: true,
-    maintainAspectRatio: false,
-  };
-
+const RecyclingChart = () => {
   return (
     <div>
-      <Doughnut data={data} options={options} />
+      <MainTopBar />
+      <div className="chart-page"></div>
     </div>
   );
 };
