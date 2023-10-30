bed955d167b1d9407bd604917db139d257fedfc8
Kevin <78829208+Kev1606@users.noreply.github.com>
2023-10-30 12:10:53 -0600

    Feat(Charts): charts for material and date completed

--- recicla-tec/src/Services/AppServices.js
+++ recicla-tec/src/Services/AppServices.js
@@ -4,9 +4,36 @@ import {
     collection,
     query,
     addDoc,
+    where,
 } from "firebase/firestore";
 const usersCollection = collection(db, "users");
 const recycledItemsCollection = collection(db, "recyclingLog");
+const mesesEnum = {
+    1: 'Jan',
+    2: 'Feb',
+    3: 'Mar',
+    4: 'Apr',
+    5: 'May',
+    6: 'June',
+    7: 'July',
+    8: 'Aug',
+    9: 'Sept',
+    10: 'Oct',
+    11: 'Nov',
+    12: 'Dic',
+};
+function obtenerColorParaMaterial(material) {
+    const colores = {
+        'Plástico': '#235434',
+        'Vidrio': '#3a7658',
+        'Papel': '#a7dcb2',
+        'Metal': '#81be4d',
+        'Café': '#c1e1b9',
+        'Electrónicos': '#e9f5db',
+        'Cartón': '#f7f7f7',
+    };
+    return colores[material] || '#000000'; // Si el material no tiene un color asignado, se usa negro
+}
 
 const AppServices = {
     async getUserCollection() {
@@ -95,7 +122,67 @@ const AppServices = {
             console.log("Error: ", error);
             return false;
         }
+    },
+    async obtenerDatosPorMes() {
+        const añoActual = new Date().getFullYear();
+        const consulta = query(
+            collection(db, 'recyclingLog'),
+            where('date', '>=', `${añoActual}-01-01`),
+            where('date', '<=', `${añoActual}-12-31`)
+        );
 
+        const querySnapshot = await getDocs(consulta);
+
+        const datosPorMes = new Array(12).fill(0);
+
+        querySnapshot.forEach((doc) => {
+            const data = doc.data();
+            const fecha = new Date(data.date);
+            const mes = fecha.getMonth() + 1;
+            const cantidad = parseFloat(data.quantity);
+
+            if (!isNaN(cantidad)) {
+                datosPorMes[mes - 1] += cantidad; // Restamos 1 para que los meses comiencen en 0
+            }
+        });
+
+        const resultado = datosPorMes.map((quantity, month) => ({ quantity, month: mesesEnum[month + 1] }));
+
+        // console.log(resultado);
+        return resultado;
+    },
+
+    async obtenerTop5Materiales() {
+        const consulta = query(collection(db, 'recyclingLog'));
+        const querySnapshot = await getDocs(consulta);
+        const datosPorMaterial = {};
+
+        querySnapshot.forEach((doc) => {
+            const data = doc.data();
+            const material = data.material;
+            const cantidad = parseFloat(data.quantity);
+
+            if (!isNaN(cantidad)) {
+                if (datosPorMaterial[material]) {
+                    datosPorMaterial[material].value += cantidad;
+                } else {
+                    datosPorMaterial[material] = {
+                        id: Object.keys(datosPorMaterial).length,
+                        value: cantidad,
+                        label: material,
+                        color: obtenerColorParaMaterial(material),
+                    };
+                }
+            }
+        });
+
+        // Ordena los materiales por cantidad en orden descendente
+        const top5Materiales = Object.values(datosPorMaterial)
+            .sort((a, b) => b.value - a.value)
+            .slice(0, 5); // Obtén los primeros 5 elementos
+
+        console.log(top5Materiales);
+        return top5Materiales;
     },
 }
 export default AppServices;
--- recicla-tec/src/pages/ChartsPage/ChartsPage.js
+++ recicla-tec/src/pages/ChartsPage/ChartsPage.js
@@ -4,6 +4,7 @@ import "./ChartsPage.css";
 import { BarChart } from "@mui/x-charts/BarChart";
 import { PieChart } from "@mui/x-charts/PieChart";
 import Typography from "@mui/material/Typography";
+import AppServices from "../../Services/AppServices";
 
 const valueFormatter = (value: number) => `${value}kg`;
 const chartSetting = {
@@ -15,56 +16,9 @@ const chartSetting = {
   width: 500,
   height: 400,
 };
-const dataset = [
-  {
-    quantity: 21,
-    month: "Jan",
-  },
-  {
-    quantity: 28,
-    month: "Fev",
-  },
-  {
-    quantity: 41,
-    month: "Mar",
-  },
-  {
-    quantity: 73,
-    month: "Apr",
-  },
-  {
-    quantity: 99,
-    month: "May",
-  },
-  {
-    quantity: 144,
-    month: "June",
-  },
-  {
-    quantity: 319,
-    month: "July",
-  },
-  {
-    quantity: 249,
-    month: "Aug",
-  },
-  {
-    quantity: 131,
-    month: "Sept",
-  },
-  {
-    quantity: 55,
-    month: "Oct",
-  },
-  {
-    quantity: 48,
-    month: "Nov",
-  },
-  {
-    quantity: 25,
-    month: "Dec",
-  },
-];
+
+const set = await AppServices.obtenerDatosPorMes();
+const data = await AppServices.obtenerTop5Materiales();
 
 const RecyclingChart = () => {
   return (
@@ -83,12 +37,7 @@ const RecyclingChart = () => {
         <PieChart
           series={[
             {
-              data: [
-                { id: 0, value: 10, label: "Plástico", color: "#235434" },
-                { id: 1, value: 15, label: "Vidrio", color: "#3a7658" },
-                { id: 2, value: 20, label: "Papel", color: "#a7dcb2" },
-                { id: 2, value: 20, label: "Metal", color: "#81be4d" },
-              ],
+              data
             },
           ]}
           width={400}
@@ -96,7 +45,7 @@ const RecyclingChart = () => {
         />
 
         <BarChart
-          dataset={dataset}
+          dataset={set}
           yAxis={[{ scaleType: "band", dataKey: "month" }]}
           series={[
             {
